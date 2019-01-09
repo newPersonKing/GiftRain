873b2bc63a5e19b2e2e4887d111879529f0b7df3
guoyong <guoyong@emcc.net.com>
2019-01-09 11:12:43 +0800

    gifrain

---
+++ app/src/main/java/top/wefor/giftrainpractice/model/BoxPrizeBean.java
@@ -0,0 +1,13 @@
+package top.wefor.giftrainpractice.model;
+
+/**
+ * Created on 2018/1/26.
+ * 礼品信息。
+ *
+ * @author ice
+ */
+
+public class BoxPrizeBean {
+    public String prizeName;
+    public int amount;
+}
